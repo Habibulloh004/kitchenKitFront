f7148ad97b7eb4177b478b60e450f568a3888d06
habibulloh004 <habibulloh4080@gmail.com>
2024-09-08 13:00:03 +0500

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -95,7 +95,7 @@ const Navbar = () => {
             }}
             className="inline-flex items-center gap-2 rounded-md bg-red-500 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-700 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
           >
-            Logout
+            Выйти
           </p>
           <div>
             {isOnline ? (
